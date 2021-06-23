69c6b496ef6ca81d80098a8ca6ab4b921248238f
enaganti6 <harish.enagante@gmail.com>
2021-06-24 03:53:51 +0530

    hello first push in feature branch

--- src/main/java/com/aadhya/springbootdemo/HelloController.java
+++ src/main/java/com/aadhya/springbootdemo/HelloController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class HelloController
-{
+public class HelloController {
+
     @GetMapping("/")
-    public String homePage()
-    {
+    public String homePage() {
         System.out.println("In controller class");
         System.out.println("In controller class");
         return "Hello Page";
---
+++ src/main/java/com/aadhya/springbootdemo/Sam.java
@@ -0,0 +1,17 @@
+package com.aadhya.springbootdemo;
+
+public class Sam {
+    public static void main(String[] args) {
+        Sam sam=new Sam();
+        sam.ruh();
+    }
+
+public void ruh()
+{
+    String name="hari";
+    if(name.equals("hari"));
+    System.out.println("running");
+}
+
+
+}
